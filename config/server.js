185e3f3434b3aad760f44a0fd19cacca48b65459
ThiagoPederzolli <thiagopederzolli@gmail.com>
2021-08-02 09:39:08 -0300

    api for won games

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '6115cf9fd43255d28941e153252cca13'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '3e11f4fd-aad7-4abb-b161-4ff43af74000'
+};
